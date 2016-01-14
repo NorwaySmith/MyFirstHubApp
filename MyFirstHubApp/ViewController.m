48f3a005b70cd7bb4b4a7b4086fd74d465923437
NorwaySmith <NorwaySmith@yahoo.com>
2016-01-14 20:50:25 +0800

    111
    22

--- MyFirstHubApp/ViewController.m
+++ MyFirstHubApp/ViewController.m
@@ -17,9 +17,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"哈哈");
+    NSLog(@"我更改了一些内容");
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+/**
+ *  添加了一个方法
+ */
+-(void)addAnCount{
+    NSLog(@"添加了一个方法");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
