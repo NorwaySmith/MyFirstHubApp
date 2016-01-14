91a4fe843c157745a275b573e917f686a5166697
NorwaySmith <NorwaySmith@yahoo.com>
2016-01-14 20:46:47 +0800

    Initial Commit

---
+++ MyFirstHubApp/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  MyFirstHubApp
+//
+//  Created by zhaotengfei on 16/1/14.
+//  Copyright © 2016年 zhaotengfei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"哈哈");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MyFirstHubApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyFirstHubApp
+//
+//  Created by zhaotengfei on 16/1/14.
+//  Copyright © 2016年 zhaotengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
